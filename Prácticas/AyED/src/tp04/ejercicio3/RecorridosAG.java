dfe453cb52c9b29e0d948b5d452607d23ab8ac28
NeoG14 <nicolaplatajob@gmail.com>
2023-04-19 05:36:17 -0300

    Ejercicio 3

--- Prácticas/AyED/src/tp04/ejercicio3/RecorridosAG.java
+++ Prácticas/AyED/src/tp04/ejercicio3/RecorridosAG.java
@@ -2,6 +2,7 @@ package tp04.ejercicio3;
 
 import tp02.ejercicio2.ListaEnlazadaGenerica;
 import tp02.ejercicio2.ListaGenerica;
+import tp02.ejercicio3.ColaGenerica;
 import tp03.ejercicio1.ArbolBinario;
 import tp04.ejercicio1.ArbolGeneral;
 
@@ -26,6 +27,30 @@ public class RecorridosAG {
 			}
 		}	
 	}
+	
+	public static ListaGenerica<Integer> numerosImparesMayoresQueInOrden(ArbolGeneral<Integer> a, Integer n) {
+		ListaGenerica<Integer> lista = new ListaEnlazadaGenerica<Integer>();
+		numerosImparesMayoresQueInOrden(a,n,lista);
+		return lista;
+	}
+		
+	private static void numerosImparesMayoresQueInOrden(ArbolGeneral<Integer> a, Integer n, ListaGenerica<Integer> l) {
+		if(a.tieneHijos()) {
+			ListaGenerica<ArbolGeneral<Integer>> lHijos = a.getHijos();
+			lHijos.comenzar();
+			if(!lHijos.fin()) {
+				numerosImparesMayoresQueInOrden(lHijos.proximo(),n,l);
+				if ( (a.getDato()>n) && (a.getDato()%2!=0) )
+					l.agregarFinal(a.getDato());
+			}
+			while(!lHijos.fin()) 
+				numerosImparesMayoresQueInOrden(lHijos.proximo(),n,l);
+		}else
+			if ( (a.getDato()>n) && (a.getDato()%2!=0) )
+				l.agregarFinal(a.getDato());
+			
+			
+	}
 		
 	public static ListaGenerica<Integer> numerosImparesMayoresQuePostOrden(ArbolGeneral<Integer> a, Integer n) {
 		ListaGenerica<Integer> lista = new ListaEnlazadaGenerica<Integer>();
@@ -40,19 +65,39 @@ public class RecorridosAG {
 			while(!lHijos.fin()) {
 				numerosImparesMayoresQuePostOrden(lHijos.proximo(),n,l);
 			}
-			if(a.getDato()>n) {
-				if(a.getDato()%2!=0)
-					l.agregarFinal(a.getDato());
-			}
+			if ( (a.getDato()>n) && (a.getDato()%2!=0) )
+				l.agregarFinal(a.getDato());
 		}else
-			if(a.esHoja()) {
-				if(a.getDato()>n) {
-					if(a.getDato()%2!=0)
-						l.agregarFinal(a.getDato());
-				}
-			}
+			if ( (a.getDato()>n) && (a.getDato()%2!=0) )
+				l.agregarFinal(a.getDato());		
 	}
 	
+	public static ListaGenerica<Integer> numerosImparesMayoresQuePorNiveles(ArbolGeneral<Integer> a, Integer n) {
+		ListaGenerica<Integer> result = new ListaEnlazadaGenerica<Integer>();
+		ColaGenerica<ArbolGeneral<Integer>> cola = new ColaGenerica<ArbolGeneral<Integer>>();
+		ArbolGeneral<Integer> arbol_aux;
+		cola.encolar(a);
+		cola.encolar(null);
+		while(!cola.esVacia()) {
+			arbol_aux = cola.desencolar();
+			if(arbol_aux != null) {
+				if(arbol_aux.getDato()>n && arbol_aux.getDato()%2!=0)
+					result.agregarFinal(arbol_aux.getDato());
+				if(arbol_aux.tieneHijos()) {
+					ListaGenerica<ArbolGeneral<Integer>> hijos = arbol_aux.getHijos();
+					hijos.comenzar();
+					while (!hijos.fin()) 
+						cola.encolar(hijos.proximo());
+				}
+			}else
+				if(!cola.esVacia())
+					cola.encolar(null);
+		}
+		return result;
+	}
+
+	
+	/*
 	public static ListaGenerica<Integer> preOrden(ArbolGeneral<Integer> a) {
 		ListaGenerica<Integer> lista = new ListaEnlazadaGenerica<Integer>();
 		preOrden(a,lista);
@@ -86,20 +131,16 @@ public class RecorridosAG {
 				 inOrden(lHijos.proximo(),l);
 			 }
 			
-		}else {
-			l.agregarFinal(a.getDato());
-		}
-		 
-		 
-		 
+		}else 
+			l.agregarFinal(a.getDato());	 
 	}
-
+	 */
 
 	
 	
 	
 	public static void main(String[] args) {
-		/*
+		
 		ArbolGeneral<Integer> a5 = new ArbolGeneral<Integer>(5);
 		ArbolGeneral<Integer> a11 = new ArbolGeneral<Integer>(11);
 		ArbolGeneral<Integer> a13 = new ArbolGeneral<Integer>(13);
@@ -113,28 +154,12 @@ public class RecorridosAG {
 		a11.agregarHijo(a9);
 		a11.agregarHijo(a17);
 		
-		ListaGenerica<Integer> l = numerosImparesMayoresQuePostOrden(a5,3);
+		ListaGenerica<Integer> l = numerosImparesMayoresQuePorNiveles(a5,3);
 		l.comenzar();
 		while(!l.fin()) {
 			System.out.println(l.proximo());
 		}
-		*/
-		ArbolGeneral<Integer> a1 = new ArbolGeneral<Integer>(1);
-		ArbolGeneral<Integer> a3 = new ArbolGeneral<Integer>(3);
-		ArbolGeneral<Integer> a4 = new ArbolGeneral<Integer>(4);
-		ArbolGeneral<Integer> a5 = new ArbolGeneral<Integer>(5);
-		ArbolGeneral<Integer> a6 = new ArbolGeneral<Integer>(6);
-		
-		a1.agregarHijo(a3);
-		a1.agregarHijo(a4);
-		a1.agregarHijo(a5);
-		a3.agregarHijo(a6);
-		a3.agregarHijo(a6);
-		
-		ListaGenerica<Integer> l = inOrden(a1);
-		while(!l.fin()) {
-			System.out.println(l.proximo());
-		}
+
 		
 	}
 }
